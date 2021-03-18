9717b60c2c7e0805c2a3d0bb27f8ef1317bad16b
Mark Noone <manoone@tcd.ie>
2021-03-18 14:47:43 +0000

    Added document header implementation

---
+++ web-client/src/doc-viewer/views/doc-header/burger_menu_button.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import styled from 'styled-components';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+type BurgerMenuButtonProps = {
+    className?: string;
+}
+
+const BurgerMenuButton: React.FunctionComponent<BurgerMenuButtonProps> = ({ className }) => 
+    <div className={className}>
+        <FontAwesomeIcon icon={faBars} size="lg"/>
+    </div>
+
+const StyledBurgerMenuButton = styled(BurgerMenuButton)`
+    width: 48px;
+    padding: 11px 0px;
+    display: inline-block;
+    text-align: center;
+    cursor: pointer;
+    background: none;
+    border-radius: 50%;
+    transform: scale(1.0);
+
+    transition: background .25s;
+    transition: scale .5s;
+    
+    &:hover {
+        background: rgba(0, 0, 0, 0.1);
+    }
+
+    &:active {
+        transform: scale(0.9);
+    }
+`;
+
+export default StyledBurgerMenuButton;
--- web-client/src/doc-viewer/views/doc-header/doc_header.tsx
+++ web-client/src/doc-viewer/views/doc-header/doc_header.tsx
@@ -1,7 +1,81 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import BurgerMenuButton from './burger_menu_button';
+
+const LayoutWrapper = styled.div`
+    width: 100%;
+    height: 64px;
+    background: #f8f8f8;
+    border-bottom: 1px solid #ddd;
+    padding: 8px 16px;
+`;
+
+const DocumentNameLayoutWrapper = styled.div`
+    display: inline-block;
+    margin-left: 12px;
+`;
+
+const DocumentName = styled.p`
+    display: inline-block;
+    font-size: 18px;
+    font-family: 'Open Sans', sans-serif;
+    user-select: none;
+    cursor: pointer;
+`;
+
+const DocumentNameInput = styled.input`
+    font-size: 18px;
+    font-family: 'Open Sans', sans-serif;
+    border-radius: 6px;
+    border: 1px solid #dedede;
+    padding: 4px 12px;
+    height: 100%;
+    outline: 0;
+`;
+
+const DocumentNameSaveButton = styled.button`
+    font-size: 14px;
+    font-weight: 600;
+    cursor: pointer;
+    color: white;
+    padding: 8px 12px;
+    border-radius: 4px;
+    border: none;
+    margin-left: 16px;
+
+    transform: scale(1.0);
+    transition: scale .5s;
+
+    &:active {
+        transform: scale(.95);
+    }
+
+    background-color: #00a8ff; 
+    font-family: 'Open Sans', sans-serif;
+`;
+
 
 const DocumentHeader : React.FunctionComponent = () => {
-    return <></>;
+    const [documentName, setDocumentName] = useState<string>("Untitled Document");
+    const [isNameBeingEdited, setIsNameBeingEdited] = useState<boolean>(false);
+    return <LayoutWrapper>
+        <BurgerMenuButton />
+        <DocumentNameLayoutWrapper>
+            {
+                isNameBeingEdited? (<>
+                    <DocumentNameInput type="text" value={documentName} 
+                        onChange={(e: React.ChangeEvent<HTMLInputElement>)=>{
+                            setDocumentName(e.target.value);
+                        }}/>
+                    <DocumentNameSaveButton onClick={() => {
+                        setIsNameBeingEdited(false);
+                    }}>Save</DocumentNameSaveButton>
+                </> ) : <DocumentName onClick={() => { 
+                    setIsNameBeingEdited(true) 
+                }}>{ documentName }</DocumentName>
+            }
+        </DocumentNameLayoutWrapper>
+    </LayoutWrapper>;
 }
 
 export default DocumentHeader;
